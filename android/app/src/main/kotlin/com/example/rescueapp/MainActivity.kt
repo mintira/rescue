6155b47977a0a87bd17109d1b4daf81ea4d7c40d
Jaruwan Saeaei <84604436+minimintorz@users.noreply.github.com>
2022-10-24 19:33:59 +0700

    create rescue app

---
+++ android/app/src/main/kotlin/com/example/rescueapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rescueapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class rescueApp extends StatelessWidget {
+  const rescueApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+
+        ),
+        body: Container(
+          
+        ),
+      ),
+    );
+  }
+}
