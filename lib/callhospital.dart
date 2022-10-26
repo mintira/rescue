887ec2660898801895602088261c7e3c9114f9d1
Jaruwan Saeaei <84604436+minimintorz@users.noreply.github.com>
2022-10-26 20:45:45 +0700

    edit call

--- lib/callhospital.dart
+++ lib/callhospital.dart
@@ -16,7 +16,62 @@ class CallHospital extends StatelessWidget {
           padding: EdgeInsets.fromLTRB(30, 10, 30, 40),
           child: ListView(
             children: [
-              
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.lightBlue[200],
+                ),
+                padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.network(
+                              'https://www.thailandplus.tv/wp-content/uploads/2019/12/%E0%B8%A3%E0%B8%9E.%E0%B8%AA%E0%B8%B8%E0%B8%A3%E0%B8%B2%E0%B8%A9%E0%B8%8E%E0%B8%A3%E0%B9%8C%E0%B8%98%E0%B8%B2%E0%B8%99%E0%B8%B5_191224_0002.jpg',
+                              width: 150),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'ชื่อโรงพยาบาล',
+                          style: GoogleFonts.kanit(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'โทรเลย',
+                        style: GoogleFonts.kanit(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue[900],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        minimumSize: Size(MediaQuery.of(context).size.width*0.8, 40),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
             ],
           ),
         ),
